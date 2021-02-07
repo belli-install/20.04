ee0b6d1c1e42c8959bb14c019f7381d782e4b251
StJ <email.stj@seznam.cz>
2021-02-07 21:51:06 +0100

    preddelani scriptu pro bankovni sudo init

--- b02-sudo-init.sh
+++ b02-sudo-init.sh
@@ -1,9 +1,70 @@
 #!/bin/bash
 
-if [ "$(whoami)" == "root" ]; then
-	echo "Nepoužívej sudo - použij pouze jako normální uživatel"
+if [ "$(whoami)" != "root" ]; then
+	echo "Chybí sudo"
 	exit 1
 fi
+
+if [ $# -lt 1 ]; then
+  echo 1>&2 "$0: Chybí argument <číslo> velikosti swapfile v GiB. Použití: sudo-init-swapfile.sh <číslo>"
+  exit 2
+elif [ $# -gt 1 ]; then
+  echo 1>&2 "$0: Příliš mnoho argumentů"
+  exit 2
+fi
+
+SWAPEXISTS=0
+
+if [ -f /swapfile ]
+then
+	echo "Nalezen aktivní swapfile. Deaktivuji a odstraňuji"
+	SWAPEXISTS=1
+	swapoff /swapfile
+	rm -rf /swapfile
+fi
+
+echo "Vytvářím swapfile"
+fallocate -l $1G /swapfile
+echo "Nastavuji swapfile"
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+echo "Swapfile nastaven a aktivován"
+
+if [ ${SWAPEXISTS} == 0 ]; then
+	echo "Zavádím swapfile do /etc/fstab"
+	sed -i '$a /swapfile   none    swap    sw    0   0' /etc/fstab
+fi
+
+
+if [ -f /etc/.jss-hostnames-added ]
+then
+    echo "Hostanames už byly nastaveny" 
+else
+	echo "Nastavuji Hostnames"
+	#nastavit bluetooth jmeno
+	echo "PRETTY_HOSTNAME=$(hostname)" | sudo tee /etc/machine-info
+
+	echo "127.0.1.1	$(hostname)" > ~/tmp_file && 
+	echo "127.0.0.1	weby" >> ~/tmp_file && 
+	cat /etc/hosts >> ~/tmp_file &&
+	mv ~/tmp_file /etc/hosts
+	
+	touch /etc/.jss-hostnames-added
+fi
+
+echo "UPNP adresář"
+mkdir /media/upnp
+chmod 777 /media/upnp
+
+# uprava grubu k zapamatovani posledni zvolene pozice
+echo "Aktivuji Savedefault v GRUBu"
+cp -vf $PWD/Data/etc/default/grub /etc/default/
+update-grub
+
+# Deaktivace popupu z Evolution kalendare
+echo "Deaktivuji otravnost kalendáře"
+cp -vf $PWD/Data/etc/xdg/autostart/org.gnome.Evolution-alarm-notify.desktop /etc/xdg/autostart/
 
 if [ -f ~/.jss-basicinit-done ]
 then
@@ -54,3 +115,4 @@ echo "Instaluji Joplin"
 wget -O - https://raw.githubusercontent.com/laurent22/joplin/dev/Joplin_install_and_update.sh | bash
 
 echo "Dokončeno"
+
