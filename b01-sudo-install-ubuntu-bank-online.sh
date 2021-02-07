3f914e2810b48ad17898bd308020e6bda09f383d
StJ <email.stj@seznam.cz>
2021-02-07 21:39:56 +0100

    Oprava chyby v instalacnim bankovnim scriptu

--- b01-sudo-install-ubuntu-bank-online.sh
+++ b01-sudo-install-ubuntu-bank-online.sh
@@ -29,8 +29,6 @@ else
 	touch /etc/.jss-software-sources-added
 fi
 
-apt-get install gparted exfat-fuse x11-utils hfsprogs p7zip p7zip-full p7zip-rar simple-scan synaptic pepperflashplugin-nonfree openjdk-14-jre openjdk-14-jre-headless openjdk-14-jdk openjdk-14-jdk-headless keepassx ubuntu-restricted-extras network-manager-openvpn-gnome -y
-
 apt-get install dconf-editor libglib2.0-dev libwxgtk3.0-gtk3-0v5 x11-utils gnome-tweaks cifs-utils net-tools samba-common samba winbind nextcloud-client chrome-gnome-shell gvfs-fuse smartmontools gsmartcontrol gparted exfat-fuse hfsprogs git p7zip p7zip-full p7zip-rar simple-scan gimagereader synaptic pepperflashplugin-nonfree openjdk-14-jre openjdk-14-jre-headless geany geany-plugins vlc keepassxc gpicview guake ubuntu-restricted-extras imagemagick doublecmd-gtk network-manager-openvpn-gnome -y
 
 apt-get update
